cfd920ee25d6287e6a359f2f897797f309bb7ca1
Hiroaki NAGOYA <h-nagoya@plamed.com>
2016-03-24 11:59:44 +0900

    index.php に「このページを開いた日時」を追加

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
+<?php
+   header('Content-Type: text/html; charset=UTF-8');
+?>
 <html>
-<body>
-<p>Hello, World!</p>
-<p>こんにちは</p>
-</body>
+  <head>
+    <title>ほげほげ</title>
+  </head>
+  <body>
+    <div>
+      <p>Hello, World!</p>
+      <p>こんにちは</p>
+    </div>
+    <div>
+      このページを開いた日時: <?php echo date('Y-m-d h:m:s P'); ?>
+    </div>
+  </body>
 </html>
