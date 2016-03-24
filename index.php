d934a8afe6eb7acc7ce08af7bbb24a685c2657ba
Hiroaki NAGOYA <h-nagoya@plamed.com>
2016-03-24 14:51:09 +0900

    index.php 日時表示修正

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 ?>
 <html>
   <head>
+    <script src="//code.jquery.com/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI=" crossorigin="anonymous"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/locale/ja.js"></script>
     <title>ほげほげ</title>
   </head>
   <body>
@@ -11,7 +14,33 @@
       <p>こんにちは</p>
     </div>
     <div>
-      このページを開いた日時: <?php echo date('Y-m-d h:m:s P'); ?>
+      <p>
+	このページを開いた日時（サーバーサイド）: <?php echo date('Y-m-d H:i:s P'); ?>
+      </p>
     </div>
+    <div>
+      <p>
+	このページを開いた日時（クライアントサイド）: <span id="ct"></span>
+      </p>
+    </div>
+    <div>
+      <p>
+	現在の日時（クライアントサイド）: <span id="ct2"></span>
+      </p>
+    </div>
+    <script type="text/javascript">
+      $(function(){
+
+      function now() {
+      	return moment().format('YYYY-MM-DD hh:mm:ss.SSS Z');
+      };
+      $("#ct").text(now());
+      $("#ct2").text(now());
+      setInterval(function() {
+      	$("#ct2").text(now());
+      }, 200);
+
+      });
+    </script>		
   </body>
 </html>
