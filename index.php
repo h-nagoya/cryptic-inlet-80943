020a3bd0701bb87cde33d4547f1429fd7960d301
Hiroaki NAGOYA <h-nagoya@plamed.com>
2016-05-09 13:49:56 +0900

    Added now.php and uptime.php

--- index.php
+++ index.php
@@ -1,57 +1,60 @@
 <?php
-   header('Content-Type: text/html; charset=UTF-8');
+header('Content-Type: text/html; charset=UTF-8');
 ?>
+<!DOCTYPE html>
 <html>
   <head>
     <script src="//code.jquery.com/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI=" crossorigin="anonymous"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/locale/ja.js"></script>
     <title>ほげほげ</title>
+    <style>
+      .b td {
+         border: solid 1px #000000;
+         border-collapse: collapse;
+         padding: 0.5em;
+      }
+</style>
   </head>
   <body>
-    <div>
-      <p>Hello, World!</p>
-      <p>こんにちは</p>
-    </div>
-    <div>
-      <p>
-	このページを開いた時点での uptime（サーバーサイド）: <?php echo shell_exec("/usr/bin/uptime"); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-	このページを開いた日時（サーバーサイド）: <?php echo date('Y-m-d H:i:s P'); ?>
-      </p>
-    </div>
-    <div>
-      <p>
-	このページを開いた日時（クライアントサイド）: <span id="ct"></span>
-      </p>
-    </div>
-    <div>
-      <p>
-	現在の日時（クライアントサイド）: <span id="ct2"></span> [<span id="ct3"></span>]
-      </p>
-    </div>
+
+    <table class="b">
+      <tr>
+	<td>このページを開いた日時（サーバーサイド）</td>
+	<td> <?php echo date('Y-m-d H:i:s P'); ?></td>
+      </tr>
+      <tr>
+	<td>このページを開いた日時（クライアントサイド）</td>
+	<td><span id="ct1"></span></td>
+      </tr>
+      <tr>
+	<td>現在の日時（クライアントサイド） </td>
+	<td><span id="ct2"></span></td>
+      </tr>
+      <tr>
+	<td>現在の日時（サーバーサイド）</td>
+	<td><span id="ct3"></span></td>
+      </tr>
+      <tr>
+	<td>uptime（サーバーサイド） </td>
+	<td><span id="ct4"></span></td>
+      </tr>
+    </table>
+
     <script type="text/javascript">
-      $(function(){
-
-      function now() {
-      	return moment().format('YYYY-MM-DD HH:mm:ss.SSS Z');
-      };
-
-      var t = now();
-      $("#ct").text(t);
-      $("#ct2").text(t);
-      $("#ct3").text(0);
-
-      var n = 0;
-      setInterval(function() {
-      	$("#ct2").text(now());
-	$("#ct3").text(++n);
-      }, 500);
-
-      });
-    </script>		
+    $(function(){
+	function now() {
+	    return moment().format('YYYY-MM-DD HH:mm:ss.SSS Z');
+	};
+	function update() {
+	    $.get('/uptime.php', function(data){$("#ct4").text(data);});
+	    $.get('/now.php', function(data){$("#ct3").text(data);});
+	    $("#ct2").text(now());
+	};
+        update();
+	$("#ct1").text(now());
+	setInterval(update, 1000);
+    });
+    </script>
   </body>
 </html>
